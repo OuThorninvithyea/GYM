3d96015f5dd7525db8b493d48810227e57ff6737
Shawnnnnhh <100029619+Shawnnnnhh@users.noreply.github.com>
2025-11-20 17:15:34 +0700

    Only treat uid as QR id when prefixed with ELIT

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -21,7 +21,7 @@ export async function GET(request: NextRequest) {
 
     let user = null;
 
-    if (uid) {
+    if (uid && /^ELIT-/i.test(uid)) {
       user = await getUserByIdAdmin(uid);
     }
 
